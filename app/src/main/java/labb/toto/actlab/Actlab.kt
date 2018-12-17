e81f40a5bf23967454f9d04bfbef60b7ceba560c
tianhao <tianhao@cmcm.com>
2018-12-18 03:23:46 +0800

    first commit

---
+++ app/src/main/java/labb/toto/actlab/Actlab.kt
@@ -0,0 +1,12 @@
+package labb.toto.actlab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Actlab : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actlab)
+    }
+}
